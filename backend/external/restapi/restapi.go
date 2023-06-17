5b3399f5dd319929d7ff718e336f70714754cd96
Muslimov Zarif <m.zarif@sarkor.uz>
2023-06-17 15:19:59 +0500

    в роут добавлен метод загрузки складов

--- backend/external/restapi/restapi.go
+++ backend/external/restapi/restapi.go
@@ -40,6 +40,7 @@ func NewRestAPI(ui uimport.UsecaseImports,
 	p.POST("/remove", api.AuthRequired(), api.RemoveProduct)
 	p.GET("/find/:id", api.AuthRequired(), api.FindProduct)
 	p.GET("/load", api.AuthRequired(), api.LoadAllProduct)
+	p.GET("/storage_list", api.AuthRequired(), api.LoadStorageList)
 
 	n := apiGroup.Group("/notification")
 	n.GET("/new", api.AuthRequired(), api.LoadMessages)
